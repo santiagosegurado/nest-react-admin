16409c321076e696d6314f9ec8a07743d9f17339
Santiago <santisegurado1@gmail.com>
2024-12-25 02:51:18 -0300

    Update proxy settings to localhost:5001 and add new custom colors in Tailwind CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandPrimary: 'var(--brand-primary)',
+        brandPrimaryDark: 'var(--brand-primary-dark)',
+        brandBackground: 'var(--brand-background)',
+        brandActive: 'var(--brand-active)',
+        brandHeaderBackground: 'var(--brand-header-background)',
+        primaryRed: 'var(--primary-red)',
+        redHover: 'var(--red-hover)',
+        primaryWhite: 'var(--primary-white)',
+        whiteHover: 'var(--white-hover)',
+      },
+    },
   },
   variants: {
     extend: {
